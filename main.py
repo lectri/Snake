57028c50f8fc00f9c59dda4093c3e86876303aac
lectri <cmade2006@gmail.com>
2021-05-19 09:59:51 -0400

    Movmement works. More info in issue #3

--- main.py
+++ main.py
@@ -5,18 +5,12 @@ from pyglet.window import key
 from pyglet import clock
 import config as c
 
-# Sprite Direction
-snakeDirection = ""
-
 # Create Window
 window = pyglet.window.Window(c.width, c.height, caption=c.caption)
 
 # Create sprite batches
 spriteBatch = pyglet.graphics.Batch()
 snakeSprite = shapes.Rectangle(x=200, y=200, width=25, height=25, color=(152, 255, 152), batch=spriteBatch)
-
-
-
 
 # Decorator Methods
 @window.event
@@ -25,29 +19,31 @@ def on_draw():
     spriteBatch.draw()
 
 @window.event
-def on_key_release(symbol, modifiers):
+def on_key_press(symbol, modifiers):
     if symbol == key.LEFT:
-        snakeDirection = "Left"
+        c.snakeDirection = "L"
     if symbol == key.RIGHT:
-        snakeDirection = "Right"
+        c.snakeDirection = "R"
     if symbol == key.UP:
-        snakeDirection = "Up"
+        c.snakeDirection = "U"
     if symbol == key.DOWN:
-        snakeDirection = "Down"
+        c.snakeDirection = "D"
+
+# Other Methods
+def move(dt):
+    if c.snakeDirection == "L":
+        snakeSprite.x -= 5
+    if c.snakeDirection == "R":
+        snakeSprite.x += 5
+    if c.snakeDirection == "U":
+        snakeSprite.y += 5
+    if c.snakeDirection == "D":
+        snakeSprite.y -= 5
     
 # NEXT UP: CREATE A FUNCTION THAT CHANGES THE PLAYERS POSTION DEPENDING ON THE SNAKEDIRECTION STRING. THEN MAKE IT A FUNCTION THAT GETS CALLED 60 TIMES A SECOND
-def move(dt):
-    if snakeDirection == "Left":
-        snakeSprite.x -= 5
-    if snakeDirection == "Right":
-        snakeSprite.x += 5
-    if snakeDirection == "Up":
-        snakeSprite.y += 5
-    if snakeDirection == "Down":
-        snakeSprite.y -= 5
 
 
-pyglet.clock.schedule_interval(move, 1/30)
+pyglet.clock.schedule_interval_soft(move, 0.1)
 
 
 # Run
