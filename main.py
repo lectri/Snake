4879c771e9cd2a7da638109f52f2eae15e45daf5
lectri <cmade2006@gmail.com>
2021-06-14 23:10:02 -0400

    fixed crashes
    Fixed app crashing on out of bounds / head running into body

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from pyglet.window import key
 from pyglet import clock
 import config as c
 
-
 # Create Window
 window = pyglet.window.Window(c.WIDTH, c.HEIGHT, caption=c.caption)
 
@@ -14,32 +13,33 @@ sprite_batch = pyglet.graphics.Batch()
 snake_sprite = shapes.Rectangle(
     x=200, y=200, width=25, height=25, color=(255, 255, 152), batch=sprite_batch)
 apple_sprite = shapes.Rectangle(x=random.randint(250, c.WIDTH), y=random.randint(
-    1, c.HEIGHT-100), width=25, height=25, color=(255, 0, 0), batch=sprite_batch)
+    1, c.HEIGHT - 100), width=25, height=25, color=(255, 0, 0), batch=sprite_batch)
 
 sprite_list = [snake_sprite]
 
 # Labels
 score_label = pyglet.text.Label(
-    f"Score = {c.LENGTH-1}",
+    f"Score = {c.LENGTH - 1}",
     font_name="Roboto",
-    font_size = 10,
+    font_size=10,
     x=10, y=10
 )
 
 death_label = pyglet.text.Label(
     font_name="Roboto",
     font_size=25,
-    x=c.WIDTH//2,
-    y=c.HEIGHT//2,
-    anchor_x = "center"
+    x=c.WIDTH // 2,
+    y=c.HEIGHT // 2,
+    anchor_x="center"
 )
 playing = True
+
 
 # Decorator Methods
 @window.event
 def on_draw():
     global playing
-    
+
     window.clear()
     if playing:
         score_label.draw()
@@ -47,7 +47,7 @@ def on_draw():
     else:
 
         death_label.draw()
-    
+
 
 # Keyboard Input Sets Snake Direction
 @window.event
@@ -71,6 +71,7 @@ def on_key_press(symbol, modifiers):
         if symbol == key.DOWN:
             c.snake_direction = "D"
 
+
 # Update
 def update(dt):
     move()
@@ -78,8 +79,9 @@ def update(dt):
     out_of_bounds()
     head_body_handle()
     snake_apple = collison_check(snake_sprite, apple_sprite)
-    if snake_apple == True:
+    if snake_apple:
         apple_snake_handle()
+
 
 # Non-Game Loop Functions
 # Checks what snake direction the snake should be, and updates accordingly
@@ -95,14 +97,15 @@ def move():
     if c.snake_direction == "D":
         snake_sprite.y -= c.SPEED_RATE
 
+
 # Checks for overlap between sprite and its target, if it meets criteria, check equals 4 and returns True
 
 
 def collison_check(sprite, target):
     if target.x < sprite.x + target.width and \
-    target.x + target.width > sprite.x and \
-    target.y < sprite.y + sprite.height and \
-    target.y + target.height > sprite.y:
+            target.x + target.width > sprite.x and \
+            target.y < sprite.y + sprite.height and \
+            target.y + target.height > sprite.y:
         return True
 
 
@@ -110,7 +113,7 @@ def follow():
     global snake_sprite
     global sprite_list
 
-    # Initalize pos with Head and Existing pos Parts
+    # giInitialize pos with Head and Existing pos Parts
     pos = [[snake_sprite.x, snake_sprite.y]]
     sprites_len = len(sprite_list)
     for i in range(sprites_len):
@@ -127,17 +130,19 @@ def follow():
     sprite_list[-1].visible = True
     return pos
 
+
 # Collision Handle: Change Apple Position
 def apple_snake_handle():
     apple_sprite.x = random.randint(100, c.WIDTH)
-    apple_sprite.y = random.randint(1, c.HEIGHT-100)
-    
+    apple_sprite.y = random.randint(1, c.HEIGHT - 100)
+
     c.LENGTH += 1
-    score_label.text = f"Score = {c.LENGTH-1}"
+    score_label.text = f"Score = {c.LENGTH - 1}"
 
     for i in range(8):
         sprite_list.append(shapes.Rectangle(
             x=-100, y=-100, width=25, height=25, color=(152, 255, 152), batch=sprite_batch))
+
 
 def head_body_handle():
     for i in sprite_list[7:]:
@@ -146,32 +151,32 @@ def head_body_handle():
             if playing == False:
                 return "Dead"
             kill()
-    
+
+
 def out_of_bounds():
     if snake_sprite.x > c.WIDTH or snake_sprite.x < 0:
         kill()
     elif snake_sprite.y > c.HEIGHT or snake_sprite.y < 0:
         kill()
 
+
 def kill():
-    print("Kill has run. Should run only ONCE.")
     global snake_sprite, apple_sprite, sprite_list, score_label, playing, out_of_bounds
     playing = False
-    pyglet.clock.unschedule(update) # Stop Updating Game
+    pyglet.clock.unschedule(update)  # Stop Updating Game
     del out_of_bounds
 
     snake_sprite.delete()
     apple_sprite.delete()
 
-    del sprite_list
-    
     window.clear()
     score_label.delete()
 
-    death_label.text = f"You Died! Your score was {c.LENGTH-1}"
+    death_label.text = f"You Died! Your score was {c.LENGTH - 1}"
+
 
 if playing:
-    pyglet.clock.schedule_interval(update, 1/60)
+    pyglet.clock.schedule_interval(update, 1 / 60)
 
 # Run
 if __name__ == "__main__":
